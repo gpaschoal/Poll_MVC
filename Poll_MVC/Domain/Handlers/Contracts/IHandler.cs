8861201b70d70f4680f360c417321a14b7527d43
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-10-18 08:37:17 -0700

    Add IHanlder contracts

---
+++ Poll_MVC/Domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,12 @@
+﻿using Poll_MVC.Domain.Commands.Contracts;
+using Poll_MVC.Domain.Responses.Contracts;
+
+namespace Poll_MVC.Domain.Handlers.Contracts
+{
+    public interface IHandler<T, R>
+            where T : ICommand
+            where R : IResponse
+    {
+        R Handle(T command);
+    }
+}
