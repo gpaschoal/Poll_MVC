5eafe039b0e0fe20f10098452ff497fcb5cc55ce
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-10-18 09:41:09 -0700

    Create commands

---
+++ Poll_MVC/Domain/Commands/CreatePollCommand.cs
@@ -0,0 +1,15 @@
+﻿using Poll_MVC.Domain.Commands.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace Poll_MVC.Domain.Commands
+{
+    public class CreatePollCommand : ICommand
+    {
+        [Required(ErrorMessage = "You should add description to your poll!")]
+        [MaxLength(150)]
+        public string Poll_description { get; set; }
+        [Required(ErrorMessage = "You should add options to your poll!")]
+        [MinLength(2)]
+        public string[] Options { get; set; }
+    }
+}
---
+++ Poll_MVC/Domain/Commands/VotePollCommand.cs
@@ -0,0 +1,15 @@
+﻿using Poll_MVC.Domain.Commands.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace Poll_MVC.Domain.Commands
+{
+    public class VotePollCommand : ICommand
+    {
+        [Required(ErrorMessage ="You should add a Poll id!")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid Poll id")]
+        public int PollId { get; set; }
+        [Required(ErrorMessage ="You should add an option id")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid Option Id")]
+        public int Option_id { get; set; }
+    }
+}
