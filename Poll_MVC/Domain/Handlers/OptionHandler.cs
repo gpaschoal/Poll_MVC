ecfb8109db7700f7b75f075212131402f833953f
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-10-18 13:12:46 -0700

    Add option and poll handler

---
+++ Poll_MVC/Domain/Handlers/OptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using Poll_MVC.Domain.Commands;
+using Poll_MVC.Domain.Handlers.Contracts;
+using Poll_MVC.Domain.Repositories;
+using Poll_MVC.Domain.Responses;
+
+namespace Poll_MVC.Domain.Handlers
+{
+    public class OptionHandler :
+        IHandler<VotePollCommand, EmptyResponse>
+    {
+        private readonly IOptionRepository _repository;
+        public OptionHandler(IOptionRepository repository) { _repository = repository; }
+
+        public EmptyResponse Handle(VotePollCommand command)
+        {
+            var option = _repository.GetById(command.PollId, command.Option_id);
+
+            if (option == null)
+                return null;
+
+            option.AddVote();
+            _repository.Update(option);
+
+            return new EmptyResponse();
+        }
+    }
+}
---
+++ Poll_MVC/Domain/Handlers/PollHandler.cs
@@ -0,0 +1,40 @@
+﻿using Poll_MVC.Domain.Commands;
+using Poll_MVC.Domain.Handlers.Contracts;
+using Poll_MVC.Domain.Models;
+using Poll_MVC.Domain.Repositories;
+using Poll_MVC.Domain.Responses;
+
+namespace Poll_MVC.Domain.Handlers
+{
+    public class PollHandler :
+        IHandler<CreatePollCommand, CreatedPollResponse>
+    {
+        private readonly IPollRepository _repository;
+        public PollHandler(IPollRepository repository) { _repository = repository; }
+
+        public CreatedPollResponse Handle(CreatePollCommand command)
+        {
+            var poll = new Poll(command.Poll_description);
+
+            foreach (var option in command.Options)
+                poll.AddOption(option);
+
+            int idPoll = _repository.Create(poll);
+
+            return new CreatedPollResponse(idPoll);
+        }
+
+        public Poll GetPoll(int idPoll)
+        {
+            var poll = _repository.GetById(idPoll);
+
+            if (poll is null)
+                return null;
+
+            poll.AddView();
+            _repository.Update(poll);
+
+            return poll;
+        }
+    }
+}
