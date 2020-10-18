9ac6f639be8b99436200ace18bd24779a1ccdf5f
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-10-18 09:48:17 -0700

    Format file

--- Poll_MVC/Domain/Commands/VotePollCommand.cs
+++ Poll_MVC/Domain/Commands/VotePollCommand.cs
@@ -5,10 +5,10 @@ namespace Poll_MVC.Domain.Commands
 {
     public class VotePollCommand : ICommand
     {
-        [Required(ErrorMessage ="You should add a Poll id!")]
+        [Required(ErrorMessage = "You should add a Poll id!")]
         [Range(0, int.MaxValue, ErrorMessage = "Please enter valid Poll id")]
         public int PollId { get; set; }
-        [Required(ErrorMessage ="You should add an option id")]
+        [Required(ErrorMessage = "You should add an option id")]
         [Range(0, int.MaxValue, ErrorMessage = "Please enter valid Option Id")]
         public int Option_id { get; set; }
     }
