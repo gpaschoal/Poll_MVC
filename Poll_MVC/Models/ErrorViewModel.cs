14c533c7498f4e1122dc959766b7e6d2ba51afec
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-10-18 08:31:30 -0700

    Added default template by microsoft

---
+++ Poll_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Poll_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
