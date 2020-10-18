292cb6d4d3efc113bce3d665a1fcac31bb8eee48
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-10-18 10:32:28 -0700

    Add dbsets and keys identifier

--- Poll_MVC/Infra/Contexts/AppDbContext.cs
+++ Poll_MVC/Infra/Contexts/AppDbContext.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Poll_MVC.Domain.Models;
 
 namespace Poll_MVC.Infra.Contexts
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options)
-            : base(options)
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            #region Identity keys
+            modelBuilder.Entity<Poll>().Property(p => p.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Option>().Property(p => p.Id).ValueGeneratedOnAdd();
+            #endregion
+
+            #region Keys
+            modelBuilder.Entity<PollView>().HasKey(x => new { x.PollId, x.CreatedAt });
+            modelBuilder.Entity<Option>().HasKey(x => new { x.PollId, x.Id });
+            modelBuilder.Entity<OptionVote>().HasKey(x => new { x.PollId, x.OptionId, x.CreatedAt });
+            #endregion
         }
+
+        public DbSet<Poll> Polls { get; set; }
+        public DbSet<Option> Options { get; set; }
     }
 }
